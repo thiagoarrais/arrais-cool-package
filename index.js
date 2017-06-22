3ea3adb2573a9a148b8b7b51a184d24deb02a673
Thiago Arrais <thiago.arrais@gmail.com>
2017-06-22 21:31:40 +0000

    just an RC

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // Main package function
 function isNullOrEmpty(input) {
-    // Returns true if the input is either undefined, null, or empty, false otherwise
-    return (input === undefined || input === null || input === '');
+    return (input === undefined || input === null);
 }
 
 // Make the main function available to other packages that require us
