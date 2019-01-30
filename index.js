338a00b299997c7c07c2c69807b8e5a9cc4b27f4
Thiago Arrais <thiago.souza@serpro.gov.br>
2019-01-30 09:16:27 -0300

    publish isNull

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 // Main package function
 function isNullOrEmpty(input) {
-    return (input === undefined || input === null || input === '');
+    return isNull(input) || isEmpty(input);
+}
+
+function isNull(input) {
+    return (input === undefined || input === null);
+}
+
+// not exported
+function isEmpty(input) {
+    return input === '';
 }
 
 // Make the main function available to other packages that require us
 module.exports = isNullOrEmpty;
+
+module.exports.isNullOrEmpty = isNullOrEmpty;
+module.exports.isNull = isNull;
